0b514c49bf41d471877e5ee6b30bf2482e27a451
dev_danny <dnyaneshghodse07@gmail.com>
2024-02-20 10:11:01 +0530

    notification layout added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,7 +9,7 @@ const AppLayout = () => {
       <Sidebar/>
       <div className="flex-1 flex flex-col dark:bg-slate-600">
         <Header />
-        <main className="flex-1">
+        <main className="flex-1 overflow-x-scroll">
           <Outlet/>
         </main>
       </div>
--- src/ui/Notifications.jsx
+++ src/ui/Notifications.jsx
@@ -1,12 +1,46 @@
-import { Card } from 'antd'
+import { Avatar, Button, Card, List } from "antd";
 
 const Notifications = () => {
+  const data = [
+    {
+      title: "Ant Design Title 1",
+    },
+    {
+      title: "Ant Design Title 2",
+    },
+    {
+      title: "Ant Design Title 3",
+    },
+    {
+      title: "Ant Design Title 4",
+    },
+  ];
   return (
     <Card>
-        <h1 className="text-xl pb-2">Notifications</h1>
-
+      <div className="flex justify-between items-center border-b pb-2">
+        <h1 className="text-xl">Notifications</h1>
+        <Button type="default">Create</Button>
+      </div>
+      <List
+        className="overflow-scroll max-h-80"
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item, index) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                />
+              }
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      />
     </Card>
-  )
-}
+  );
+};
 
-export default Notifications
+export default Notifications;
